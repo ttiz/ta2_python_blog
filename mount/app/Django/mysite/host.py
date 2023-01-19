b3da0155e355de17e32d96607dddcdbb5c460b0a
ttiz <tatsukichisan@protonmail.com>
2023-01-19 17:53:44 +0900

    socialアプリの追加

--- mount/app/Django/mysite/host.py
+++ mount/app/Django/mysite/host.py
@@ -4,4 +4,5 @@ from django_hosts import patterns, host
 host_patterns = patterns('',
                          host('', settings.ROOT_URLCONF, name=''),
                          host('tech', 'tech.urls', name='tech'),
+                         host('social', 'social.urls', name='social'),
                          )
