0210a4c61d4deac22947cc14d07391d21a698665
ttiz <tatsukichisan@protonmail.com>
2023-02-06 01:26:16 +0900

    #3

--- mount/app/Django/mysite/host.py
+++ mount/app/Django/mysite/host.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django_hosts import patterns, host
 
 host_patterns = patterns('',
-                         host('', settings.ROOT_URLCONF, name=''),
+                         # host('', settings.ROOT_URLCONF, name=''),
                          host('tech', 'tech.urls', name='tech'),
                          host('social', 'social.urls', name='social'),
                          )
